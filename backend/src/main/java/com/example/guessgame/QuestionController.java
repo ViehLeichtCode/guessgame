6dc195b32844a9a64168388953562158c5ef0991
Unsinnigkeit <toastbrotjustin@gmail.com>
2025-08-24 17:50:42 +0200

    add backend

---
+++ backend/src/main/java/com/example/guessgame/QuestionController.java
@@ -0,0 +1,24 @@
+package com.example.guessgame;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/questions")
+public class QuestionController {
+
+    @Autowired
+    private QuestionRepository repo;
+
+    @GetMapping
+    public List<Question> getAll() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public Question create(@RequestBody Question q) {
+        return repo.save(q);
+    }
+}
